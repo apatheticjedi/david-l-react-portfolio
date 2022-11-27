e47470c243091a3da53e23f77aac04f4846cbef8
Duder <thedudeabides1981@hotmail.com>
2022-11-26 23:51:47 -0800

    added pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                I am currently attending a coding boot camp through the University of Oregon in Full Stack Web Development. I am looking to become a web developer once I am finished. I have a diverse background in media production, career services, recruiting, and sales. I am looking to apply my creativity and motivation in pursuit of this new career.
+            </p>
+            <p>
+                My interests include astronomy, mycology, film production, and board games. I enjoy playing and creating board games in my free time. I also love watching movies.
+            </p>
+        </div>
+    );
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>David Lundt</h1>
+            <h2>Full Stack Programmer</h2>
+            
+        </div>
+    );
+}
